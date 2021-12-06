7746472e3ba0cc33065dcdb0706142c11b00808f
Huy Nguyen <huynv@mz.co.kr>
2021-12-06 11:46:44 +0700

    change project name

--- src/main/java/com/lguplus/fleta/SmartuxAdmApplication.java
+++ src/main/java/com/lguplus/fleta/SmartuxAdmApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BffApplication {
+public class SmartuxAdmApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BffApplication.class, args);
+		SpringApplication.run(SmartuxAdmApplication.class, args);
 
 
 	}
--- src/main/java/com/lguplus/fleta/ports/test/TestController.java
+++ src/main/java/com/lguplus/fleta/ports/test/TestController.java
@@ -1,5 +1,7 @@
 package com.lguplus.fleta.ports.test;
 
+import com.lguplus.fleta.config.dto.UserInfo;
+import com.lguplus.fleta.ports.service.UserService;
 import io.swagger.annotations.Api;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -8,9 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
 @Api(value = "TestController", description = "REST Apis related to Student Entity!!!!")
 @RestController
 public class TestController {
+    private UserService userService;
     @RequestMapping(method = RequestMethod.GET, value = "/test/hello")
     public String sayHello() {
-
+//        Ser
+        UserInfo rs =  userService.findUser("admin");
+        System.out.println("Result =============== " + rs.getUsername());
         return "Swagger Hello World";
     }
 
